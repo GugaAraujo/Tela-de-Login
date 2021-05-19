80f9569a077c523f2f9518dc27f63962a3d490a8
Gustavo Araújo <gustavo.avk@msn.com>
2021-05-19 00:13:35 -0300

    Add files via upload
    Padding adicionado ao Input, ao invés de apenas no Placeholder.
    Utilizando condicional para alterar password para texto e vice-versa com evento de Click e TouchStart

--- form.js
+++ form.js
@@ -1,28 +1,35 @@
-document.getElementById('olho').addEventListener('mousedown', function() {
-    document.getElementById('senha').type = 'text';
+//utilizando condicional para converter o type de Password para Text e vice-versa
+
+document.getElementById('olho').addEventListener('click', function() {
+    if (document.getElementById('senha').type == 'password'){
+      document.getElementById('senha').type = 'text';
+    }
+    else if (document.getElementById('senha').type == 'text') {
+      document.getElementById('senha').type = 'password';
+    }
   });
-  
-  document.getElementById('olho').addEventListener('mouseup', function() {
-    document.getElementById('senha').type = 'password';
-  });
-  
+    
   // Para que o password não fique exposto apos mover a imagem.
   document.getElementById('olho').addEventListener('mousemove', function() {
     document.getElementById('senha').type = 'password';
   });
 
-
   document.getElementById('olho').addEventListener('touchstart', function() {
-    document.getElementById('senha').type = 'text';
+    if (document.getElementById('senha').type == 'password'){
+      document.getElementById('senha').type = 'text';
+    }
+    else if (document.getElementById('senha').type == 'text') {
+      document.getElementById('senha').type = 'password';
+    }
   });
 
-  document.getElementById('olho').addEventListener('touchend', function() {
-    document.getElementById('senha').type = 'password';
-  });
+  // document.getElementById('olho').addEventListener('touchend', function() {
+  //   document.getElementById('senha').type = 'password';
+  // });
 
-  document.getElementById('olho').addEventListener('touchemove', function() {
-    document.getElementById('senha').type = 'password';
-  });
+  // document.getElementById('olho').addEventListener('touchemove', function() {
+  //   document.getElementById('senha').type = 'password';
+  // });
 
   document.getElementById('olho').ondragstart = function() { return false; };
 
